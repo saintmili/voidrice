2b1a020215c3a2b6ed95dbdb60e74e8d9c5d4eae
Mili <m.ahrari77@gmail.com>
2023-12-29 02:10:08 +0330

    move undodir to cache

--- .config/nvim/lua/saintmili/set.lua
+++ .config/nvim/lua/saintmili/set.lua
@@ -16,7 +16,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
@@ -31,5 +31,5 @@ vim.opt.cursorline = true
 
 vim.opt.updatetime = 50
 
-vim.cmd[[colorscheme dracula]]
-vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[colorscheme dracula]]
+vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
